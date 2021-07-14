dd616f0b0323b8bd6002338257e0dddfdd8afa37
kash-if <shaikhkashif064@gmail.com>
2021-07-14 20:05:56 +0530

    Task Completed: Get input from UI & insert item to data structure. Also populate UI item collection from data structure

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Storage Controller
 
+
+
 // Item Controller
 const ItemCtrl = (function() {
   // Item constructor
@@ -8,33 +10,83 @@ const ItemCtrl = (function() {
     this.name = name;
     this.calories = calories;
   }
-  
+
   // Data structure / State
   const data = {
-    items: [
-      {id: 1, name: 'Steak Dinner', calories: 1200},
-      {id: 2, name: 'Burger', calories: 450},
-      {id: 3, name: 'Ice Cream', calories: 150}
-    ],
+    items: [],
     currentItem: null,
     totalCalories: 0
   }
-  
+
   // Public methods
   return {
     logData: function() {
       return data;
+    },
+    getItems: function() {
+      return data.items;
+    },
+    addItem: function(name, calories) {
+      let ID;
+      if (data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      newItem = new Item(ID, name, calories);
+      data.items.push(newItem);
     }
   }
 })();
 
 
+
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+      itemList: '#item-list',
+      addBtn: '.add-btn',
+      itemNameInput: '#item-name',
+      itemCaloriesInput: '#item-calories'
+  }
+
+  // Function to display data collection in UI list items
+  const populateItems = function(items) {
+    let html = '';
+
+    // Looping through items 
+    items.forEach(function(item) {
+      html += `
+      <li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong><em>${item.calories} calories</em>
+        <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil-alt"></i></a>
+      </li>
+      `;
+    });
+
+    // Display list items in UI
+    document.querySelector(UISelectors.itemList).innerHTML = html;
+  }
   
   // Public methods
   return {
-    
+    // Function to return (variables - UI selectors) pair
+    getUISelectors: function() {
+      return UISelectors;
+    },
+    populateItemList: function(items) {
+      populateItems(items);
+    },
+    getItemInput: function() {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value
+      }
+    },
+    clearInput: function() {
+      document.querySelector(UISelectors.itemNameInput).value = '';
+      document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    }
   }
 })();
 
@@ -42,12 +94,41 @@ const UICtrl = (function() {
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl) {
-  
+  // Load event listeners
+  const loadEventListeners = function() {
+    // Get UI selectors from UI controller module
+    const UISelectors = UICtrl.getUISelectors();
+
+    // Add input event
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+  }
+
+  const itemAddSubmit = function(e) {
+    // Get input data from UI controller
+    const input = UICtrl.getItemInput();
+
+    // Check whether input data is null
+    if(input.name !== '' && input.calories !== '') {
+      // Add new item to data through Item controller
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+      UICtrl.clearInput();
+    }
+
+    e.preventDefault();
+  }
+
   // Public methods
   return {
     init: function() {
       // Log Data in console log (debug mode)
       console.log(ItemCtrl.logData());
+      // Fetch data from data structure in Item controller module
+      const items = ItemCtrl.getItems();
+      // Display list items in UI through UI controller module
+      UICtrl.populateItemList(items);
+      // Load Event Listeners
+      loadEventListeners();
     }
   }
 })(ItemCtrl, UICtrl);
